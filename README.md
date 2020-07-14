d8af62a69a710a7f501c76aca16fc908f90aff87
jack-oram <50764842+jack-oram@users.noreply.github.com>
2020-07-14 17:19:07 +1200

    submit button

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Phone
  phone comparison website for DTG300
+
+Open XAMPP - Start Apache and MySQL
+Open http://localhost/phone/phone/
--- index.php
+++ index.php
@@ -4,43 +4,47 @@
 
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
+<form action="compare.php">
+    <table name = "Phones">
+            <tr>
+                <th>ID</th>
+                <th>Model</th>
+                <th>Price</th>
+            </tr>
+        <?php
+            $connection = mysqli_connect('localhost', 'root', '', 'phones');
+            // Check connection
+            if (!$connection) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
 
-<table name = "Phones">
-        <tr>
-            <th>ID</th>
-            <th>Model</th>
-            <th>Price</th>
-        </tr>
-<?php
-    $connection = mysqli_connect('localhost', 'root', '', 'phones');
-    // Check connection
-    if (!$connection) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+            $sql = "SELECT ID, Model, Price FROM phones";
+            $result = mysqli_query($connection, $sql);
 
-    $sql = "SELECT ID, Model, Price FROM phones";
-    $result = mysqli_query($connection, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while($row = mysqli_fetch_assoc($result)) {
+                echo "<tr> <th><input type='checkbox'></th><th> " . $row["Model"]. "</th><th> " . $row["Price"]. "</th></tr>";
 
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-          echo "<tr> <th><input type='checkbox'></th><th> " . $row["Model"]. "</th><th> " . $row["Price"]. "</th></tr>";
+                }
+            } else {
+                echo "0 results";
+            }
+            
+            mysqli_close($connection);
 
-        }
-    } else {
-        echo "0 results";
-    }
-      
-    mysqli_close($connection);
-
-    
-    
-?>
+            
+            
+        ?>
 
 
 
 
     </table>
+
+    <input type="submit">
+
+</form>
 
 </body>
 <html/>
