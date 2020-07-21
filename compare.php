447e5ce805eca74f29bfa4aa695fbb80cdefacba
jack-oram <50764842+jack-oram@users.noreply.github.com>
2020-07-21 20:02:36 +1200

    query to get selected phones

--- compare.php
+++ compare.php
@@ -9,6 +9,29 @@ Welcome
 
 
 ?><br>
+<?php
+    $connection = mysqli_connect('localhost', 'root', '', 'phones');
+                // Check connection
+                if (!$connection) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "SELECT * FROM phones where ID IN (" . implode(",",$_POST['check_list']) . ")";
+                $result = mysqli_query($connection, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr> <th>";
+                    echo "</th><th> " . $row["Model"]. "</th><th> " . $row["Price"]. "</th></tr>";
+
+                    }
+                } else {
+                    echo "0 results";
+                }
+                
+                mysqli_close($connection);
+?>
 
 </body>
 </html>
