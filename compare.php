0b0dc5e52d61db502b3e8948d3001edd12937162
jack-oram <50764842+jack-oram@users.noreply.github.com>
2020-08-16 13:23:03 +1200

    fixed compare page

--- compare.php
+++ compare.php
@@ -1,37 +1,6 @@
 <html>
 <body>
 
-Welcome 
-<?php
-    foreach($_POST['check_list'] as $selected) {
-        echo "<p>".$selected ."</p>";
-    }
-
-
-?><br>
-<?php
-    $connection = mysqli_connect('localhost', 'root', '', 'phones');
-                // Check connection
-                if (!$connection) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
-
-                $sql = "SELECT * FROM phones where ID IN (" . implode(",",$_POST['check_list']) . ")";
-                $result = mysqli_query($connection, $sql);
-
-                if (mysqli_num_rows($result) > 0) {
-                    // output data of each row
-                    while($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr> <th>";
-                    echo "</th><th> " . $row["Model"]. "</th><th> " . $row["Price"]. "</th></tr>";
-
-                    }
-                } else {
-                    echo "0 results";
-                }
-                
-                mysqli_close($connection);
-?>
 
 <h1>My First Heading</h1>
 <?php
