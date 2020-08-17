7fa7f71d683af26440a6b1a098d2c918afb07c72
jack-oram <50764842+jack-oram@users.noreply.github.com>
2020-08-17 19:43:28 +1200

    Using CSS to make it look more appealing

--- compare.php
+++ compare.php
@@ -1,32 +1,36 @@
 <html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
 <body>
+<div class="container">
 
+    <h1>My First Heading</h1>
+    <?php
+        if (count($_POST['check_list']) == 0) {
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+            exit();
+        }
+    ?>
+    <?php
+        $connection = mysqli_connect('localhost', 'root', '', 'phones');
+        $elements = array("Model", "Price", "Storage", "Camera Resolution", "5G enabled", "Size of Phone", "Operating System");
+        echo "<table>";
+        foreach ($elements as $value) {
+            $sql = "SELECT * FROM phones where ID IN (" . implode(",",$_POST['check_list']) . ")";
+            $result = mysqli_query($connection, $sql);
+        echo "<tr>" ;
+        echo "<td> " . $value . " </td>" ;
+        while($row = $result->fetch_assoc()) {
+            echo "<td> " . $row[$value] . " </td>" ;
+        }
+        echo "</tr>";
+        };
+        echo "</table>";
 
-<h1>My First Heading</h1>
-<?php
-    if (count($_POST['check_list']) == 0) {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-        exit();
-    }
-?>
-<?php
-    $connection = mysqli_connect('localhost', 'root', '', 'phones');
-    $elements = array("Model", "Price", "Storage", "Camera Resolution", "5G enabled", "Size of Phone", "Operating System");
-    echo "<table>";
-    foreach ($elements as $value) {
-        $sql = "SELECT * FROM phones where ID IN (" . implode(",",$_POST['check_list']) . ")";
-        $result = mysqli_query($connection, $sql);
-       echo "<tr>" ;
-       echo "<td> " . $value . " </td>" ;
-       while($row = $result->fetch_assoc()) {
-        echo "<td> " . $row[$value] . " </td>" ;
-       }
-       echo "</tr>";
-     };
-    echo "</table>";
+        mysqli_close($connection);
 
-    mysqli_close($connection);
-
-?>
+    ?>
+</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,52 +1,65 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+        </div>
+        <div class="col">
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-<form action="compare.php" method="post">
-    <table name = "Phones">
-            <tr>
-                <th>ID</th>
-                <th>Model</th>
-                <th>Price</th>
-            </tr>
-        <?php
-            $connection = mysqli_connect('localhost', 'root', '', 'phones');
-            // Check connection
-            if (!$connection) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
+            <h1>Phone Comparision Tool</h1>
+            <p>Click on the phones you want to compare then click compare!</p>
+            <form action="compare.php" method="post">
+                <table name = "Phones">
+                        <tr>
+                            <th></th>
+                            <th>Model</th>
+                            <th>Price</th>
+                        </tr>
+                    <?php
+                        $connection = mysqli_connect('localhost', 'root', '', 'phones');
+                        // Check connection
+                        if (!$connection) {
+                            die("Connection failed: " . mysqli_connect_error());
+                        }
 
-            $sql = "SELECT ID, Model, Price FROM phones";
-            $result = mysqli_query($connection, $sql);
+                        $sql = "SELECT ID, Model, Price FROM phones";
+                        $result = mysqli_query($connection, $sql);
 
-            if (mysqli_num_rows($result) > 0) {
-                // output data of each row
-                while($row = mysqli_fetch_assoc($result)) {
-                echo "<tr> <th>";
-                echo "<input type='checkbox' value=" . $row["ID"] . " name='check_list[]'>";
-                echo "</th><th> " . $row["Model"]. "</th><th> " . $row["Price"]. "</th></tr>";
+                        if (mysqli_num_rows($result) > 0) {
+                            // output data of each row
+                            while($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr> <th>";
+                            echo "<input type='checkbox' value=" . $row["ID"] . " name='check_list[]'>";
+                            echo "</th><th> " . $row["Model"]. "</th><th> " . $row["Price"]. "</th></tr>";
 
-                }
-            } else {
-                echo "0 results";
-            }
-            
-            mysqli_close($connection);
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        
+                        mysqli_close($connection);
 
-            
-            
-        ?>
-
-
+                        
+                        
+                    ?>
 
 
-    </table>
 
-    <input type="submit">
 
-</form>
+                </table>
+
+                <input type="submit" class="btn btn-primary" value="Compare">
+
+            </form>
+        </div>
+        <div class="col">
+        </div>
+    </div>
+</div>
 
 </body>
-<html/>
+</html>
