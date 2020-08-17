3d09449384a89cdc7de625fb057b47b3376fb585
jack-oram <50764842+jack-oram@users.noreply.github.com>
2020-08-17 20:12:04 +1200

    formated the comparison page

--- compare.php
+++ compare.php
@@ -4,33 +4,41 @@
 </head>
 <body>
 <div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="container">
 
-    <h1>My First Heading</h1>
-    <?php
-        if (count($_POST['check_list']) == 0) {
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
-            exit();
-        }
-    ?>
-    <?php
-        $connection = mysqli_connect('localhost', 'root', '', 'phones');
-        $elements = array("Model", "Price", "Storage", "Camera Resolution", "5G enabled", "Size of Phone", "Operating System");
-        echo "<table>";
-        foreach ($elements as $value) {
-            $sql = "SELECT * FROM phones where ID IN (" . implode(",",$_POST['check_list']) . ")";
-            $result = mysqli_query($connection, $sql);
-        echo "<tr>" ;
-        echo "<td> " . $value . " </td>" ;
-        while($row = $result->fetch_assoc()) {
-            echo "<td> " . $row[$value] . " </td>" ;
-        }
-        echo "</tr>";
-        };
-        echo "</table>";
+                <h1>Phone Comparision</h1>
+                <table class="table">
+                    <tbody>
+                        <?php
+                            if (count($_POST['check_list']) == 0) {
+                                header('Location: ' . $_SERVER['HTTP_REFERER']);
+                                exit();
+                            }
+                        ?>
+                        <?php
+                            $connection = mysqli_connect('localhost', 'root', '', 'phones');
+                            $elements = array("Model", "Price", "Storage", "Camera Resolution", "5G enabled", "Size of Phone", "Operating System");
+                            echo "<table>";
+                            foreach ($elements as $value) {
+                                $sql = "SELECT * FROM phones where ID IN (" . implode(",",$_POST['check_list']) . ")";
+                                $result = mysqli_query($connection, $sql);
+                            echo "<tr>" ;
+                            echo "<th scope ='row'> " . $value . " </th>" ;
+                            while($row = $result->fetch_assoc()) {
+                                echo "<td> " . $row[$value] . " </td>" ;
+                            }
+                            echo "</tr>";
+                            };
+                            echo "</table>";
 
-        mysqli_close($connection);
+                            mysqli_close($connection);
 
-    ?>
-</div>
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 </body>
 </html>
